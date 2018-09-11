268d1567ea6b441b14f0a09863ef5e79fa2f99cc
Dan.Kapustin <dan.kapustin@gmail.com>
2018-09-11 23:12:33 +0300

    add DataClass. And annuitet function

--- loan_calc/DataClass.swift
+++ loan_calc/DataClass.swift
@@ -10,24 +10,33 @@ import Foundation
 
 class LoanCalc {
     //parameters for calculation
-    var FirstPay: Float?
-    var Amount: Float?
-    var Rate: Float?
-    var MonthsPeriod: Int?
-    var TypePay: Int?
-    var StartMonth: String?
-    var StartYear: String?
+    var paramsAmount: [String: Double]
+    var paramsType: [String: Int]
     
     //results of calculation
-    var MonthlyPayment: Float?
+    var result: [String: Double]
     
     init() {
-        FirstPay = 0.0
-        Amount = 0.0
-        Rate = 0.0
-        MonthsPeriod = 0
-        TypePay = 0
+        self.paramsAmount = ["FirstPay": 0.00, "Amount": 0.00, "Rate": 0.00]
+        self.paramsType = ["MonthsPeriod": 0, "TypePay": 0]
+        self.result = ["MonthlyPayment": 0.00, "OverPaymentAmount": 0.00]
     }
     
+    func annuitetCalculation() {
+        //Условие: сумма кредита — 1 000 000 рублей, срок — три года (36 месяцев), ставка — 20%. Погашение осуществляется аннуитетными платежами.
+        //1. Процентная ставка по кредиту в месяц = годовая процентная ставка / 12 месяцев. Пример: 20%/12 месяцев/100=0,016.
+        let RateMonth: Double = self.paramsAmount["Rate"]! / 12.00 / 100.00
+        
+        //2. Коэффициент аннуитета = (0,016*(1+0,016)^36)/((1+0,016)^36—1)=0,03676.
+        let monthPeriod = self.paramsType["MonthPeriod"]!
+        let power = NSDecimalNumber(decimal: pow(Decimal(1+RateMonth), Int(monthPeriod))).doubleValue
+        let CoefAnnuitet: Double = (RateMonth * Double(power)) / (Double(power)-1)
+        
+        //3. Ежемесячный аннуитетный платеж = 0,03676*1 000 000 рублей = 37 163,58 рубля.
+        self.result["MonthlyPayment"] = self.paramsAmount["Amount"]! * CoefAnnuitet
+        
+        //4. Итого переплата по кредиту составила 338 000 рублей. Платеж * срок - Сумма кредита
+        self.result["OverPaymentAmount"] = self.result["MonthlyPayment"]! * Double(self.paramsType["MonthsPeriod"]!) - self.paramsAmount["Amount"]!
+    }
     
 }
